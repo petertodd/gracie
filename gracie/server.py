a36e7a1ebb8eb0c3f8dcd0fb9d0c6a6f61a078f4
Ben Finney <ben@benfinney.id.au>
2007-03-06 01:07:39 +1100

    Test and implement server response to requests
    * gracie/test/test_server.py, gracie/server.py:
      - Use logging system to log messages during request handling
      - Handle GET case where asking for identity page
      - Make identity page data by delegating to Page class
      - Write response header and body back to TCP connection
      - Recognise that the handler invokes handle() when instantiated

--- gracie/server.py
+++ gracie/server.py
@@ -11,13 +11,20 @@
 """ Behaviour for OpenID provider server
 """
 
+import sys
 import logging
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+import urlparse
+import traceback
+
+Page = object
 
 __version__ = "0.0"
 
 # Name of the Python logging instance to use for this module
 logger_name = "gracie.server"
+
+from http_response import HTTPResponseCodes as HTTPCodes
 
 
 class HTTPServer(HTTPServer, object):
@@ -42,7 +49,35 @@ class OpenIDRequestHandler(BaseHTTPRequestHandler):
     def log_message(self, format, *args, **kwargs):
         """ Log a message via the server's logger """
         logger = self._server.logger
-        logger.log(format, *args, **kwargs)
+        loglevel = logging.INFO
+        logger.log(loglevel, format, *args, **kwargs)
+
+    def _get_url_params(self):
+        """ Get the component parameters from the requested URL """
+        keys = [
+            'scheme', 'location', 'path', 'query', 'fragment_id',
+        ]
+        values = urlparse.urlsplit(self.path)
+        params = dict(zip(keys, values))
+        return params
+
+    def do_GET(self):
+        """ Handle a GET command """
+        self.url_params = self._get_url_params()
+        path = self.url_params['path']
+        if path.startswith("/id/"):
+            identity = path[4:]
+            page_data = self._make_identity_page(identity)
+            self.send_response(HTTPCodes.ok)
+            self.end_headers()
+            self.wfile.write(page_data)
+
+    def _make_identity_page(self, identity):
+        """ Construct a page for an identity """
+        page_title = "Identity page for %(identity)s" % locals()
+        page = self.server.PageClass(title=page_title)
+        page_data = page.serialise()
+        return page_data
 
 
 class OpenIDServer(HTTPServer):
@@ -54,6 +89,7 @@ class OpenIDServer(HTTPServer):
         super(OpenIDServer, self).__init__(
             server_address, RequestHandlerClass
         )
+        self.PageClass = Page
 
     def _setup_logging(self):
         """ Set up logging for this server """
--- gracie/test/test_server.py
+++ gracie/test/test_server.py
@@ -15,6 +15,7 @@
 import sys
 import os
 from StringIO import StringIO
+import logging
 
 import scaffold
 from scaffold import Mock
@@ -28,12 +29,32 @@ class Stub_Logger(object):
     def log(self, format, *args, **kwargs):
         """ Log a message """
 
+class Stub_Page(object):
+    """ Stub class for Page """
+    def __init__(self, title):
+        """ Set up a new instance """
+    def serialise(self):
+        """ Serialise the page content """
+
 class Stub_OpenIDServer(object):
     """ Stub class for OpenIDServer """
 
     def __init__(self):
         """ Set up a new instance """
         self.logger = Stub_Logger()
+        self.PageClass = Stub_Page
+
+class Stub_EmptyFile(object):
+    """ Stub class for a null file
+
+    Always empty when read
+    """
+    def __init__(self): self.closed = False
+    def flush(self): pass
+    def close(self): self.closed = True
+    def next(self): raise StopIteration
+    def read(self, size=None): return ""
+    def readline(self, size=None): return ""
 
 class Stub_TCPConnection(object):
     """ Stub class for TCP connection """
@@ -44,7 +65,13 @@ class Stub_TCPConnection(object):
 
     def makefile(self, mode, bufsize):
         """ Make a file handle to the connection stream """
-        return StringIO(self._text)
+        conn_file = None
+        if mode.startswith('r'):
+            conn_file = StringIO(self._text)
+            conn_file.seek(0)
+        elif mode.startswith('w'):
+            conn_file = StringIO("")
+        return conn_file
 
 class Test_OpenIDRequestHandler(scaffold.TestCase):
     """ Test cases for OpenIDRequestHandler class """
@@ -60,13 +87,22 @@ class Test_OpenIDRequestHandler(scaffold.TestCase):
         self.valid_requests = {
             'simple': dict(
             ),
-            'get_foo': dict(
+            'get-foo': dict(
                 request_text = "GET /foo HTTP/1.1",
                 command = "GET",
                 path = "/foo",
                 version = "HTTP/1.1",
             ),
+            'id-fred': dict(
+                request_text = "GET /id/fred HTTP/1.1",
+                command = "GET",
+                path = "/id/fred",
+                version = "HTTP/1.1",
+            ),
         }
+
+        logging.basicConfig(stream=self.test_stdout)
+        test_logger = logging.getLogger(server.logger_name)
 
         for key, params in self.valid_requests.items():
             args = params.get('args')
@@ -76,14 +112,15 @@ class Test_OpenIDRequestHandler(scaffold.TestCase):
             address = params.setdefault('address', ("", 0))
             http_server = params.setdefault('server',
                                             Stub_OpenIDServer())
+            http_server.logger = test_logger
+            http_server.PageClass = Stub_Page
             if not args:
                 args = dict(
                     request = request,
                     client_address = address,
                     server = http_server,
                 )
-            instance = self.handler_class(**args)
-            params['instance'] = instance
+            params['args'] = args
 
         self.iterate_params = scaffold.make_params_iterator(
             default_params_dict = self.valid_requests
@@ -94,6 +131,12 @@ class Test_OpenIDRequestHandler(scaffold.TestCase):
         python_version = sys.version.split()[0]
         self.expect_sys_version = "Python/%(python_version)s" % locals()
 
+    def make_instance_from_args(self, args, handler_class=None):
+        """ Construct an instance from the specified arguments """
+        if handler_class is None:
+            handler_class = self.handler_class
+        return handler_class(**args)
+
     def tearDown(self):
         """ Tear down test fixtures """
         sys.stdout = self.stdout_prev
@@ -101,8 +144,15 @@ class Test_OpenIDRequestHandler(scaffold.TestCase):
     def test_instantiate(self):
         """ New OpenIDRequestHandler instance should be created """
         for key, params in self.iterate_params():
-            instance = params['instance']
+            instance = self.make_instance_from_args(params['args'])
             self.failUnless(instance is not None)
+
+    def test_server_as_specified(self):
+        """ OpenIDRequestHandler should have specified server attribute """
+        for key, params in self.iterate_params():
+            instance = self.make_instance_from_args(params['args'])
+            http_server = params['server']
+            self.failUnlessEqual(http_server, instance.server)
 
     def test_server_version_as_specified(self):
         """ OpenIDRequestHandler should report module version """
@@ -111,26 +161,28 @@ class Test_OpenIDRequestHandler(scaffold.TestCase):
 
     def test_version_string_as_specified(self):
         """ OpenIDRequestHandler should report expected version string """
+        params = self.valid_requests['simple']
+        instance = self.make_instance_from_args(params['args'])
         expect_sys_version = self.expect_sys_version
         expect_server_version = self.expect_server_version
         expect_version_string = (
             "%(expect_server_version)s %(expect_sys_version)s"
             % locals() )
-        instance = self.valid_requests['simple']['instance']
         version_string = instance.version_string()
         self.failUnlessEqual(expect_version_string, version_string)
 
     def test_log_message_to_logger(self):
         """ Request should log messages using server's logger """
         params = self.valid_requests['simple']
-        instance = params['instance']
+        instance = self.make_instance_from_args(params['args'])
         http_server = params['server']
         http_server.logger = Mock("logger")
         http_server.logger.log = Mock("logger.log")
         msg_format = "Foo"
+        msg_level = logging.INFO
         msg_args = ("spam", "eggs")
         expect_stdout = """\
-            Called logger.log(%(msg_format)r, ...)
+            Called logger.log(%(msg_level)r, %(msg_format)r, ...)
             """ % locals()
         instance.log_message(msg_format, *msg_args)
         self.failUnlessOutputCheckerMatch(
@@ -143,8 +195,8 @@ class Test_OpenIDRequestHandler(scaffold.TestCase):
             request_text = params.get('request_text')
             if not request_text:
                 continue
-            instance = params['instance']
             command = params['command']
+            instance = self.make_instance_from_args(params['args'])
             self.failUnlessEqual(command, instance.command)
 
     def test_path_from_request(self):
@@ -153,8 +205,8 @@ class Test_OpenIDRequestHandler(scaffold.TestCase):
             request_text = params.get('request_text')
             if not request_text:
                 continue
-            instance = params['instance']
             path = params['path']
+            instance = self.make_instance_from_args(params['args'])
             self.failUnlessEqual(path, instance.path)
 
     def test_request_version_from_request(self):
@@ -163,9 +215,50 @@ class Test_OpenIDRequestHandler(scaffold.TestCase):
             request_text = params.get('request_text')
             if not request_text:
                 continue
-            instance = params['instance']
             request_version = params['version']
+            instance = self.make_instance_from_args(params['args'])
             self.failUnlessEqual(request_version, instance.request_version)
+
+    def test_request_get_id_returns_user_page(self):
+        """ Request for user ID should return identity page """
+        class Mock_TCPConnection(object):
+            """ Allow writing to a mock connection """
+            def __init__(self, text):
+                self._text = text
+            def makefile(self, mode, bufsize):
+                """ Make a file handle to the connection stream """
+                conn_file = None
+                if mode.startswith('r'):
+                    conn_file = StringIO(self._text)
+                    conn_file.seek(0)
+                elif mode.startswith('w'):
+                    conn_file = Mock('TCPConnection.wfile')
+                return conn_file
+                
+        params = self.valid_requests['id-fred']
+        args = params['args']
+        request = Mock_TCPConnection(params['request_text'])
+        args['request'] = request
+        Page = Mock('Page')
+        Page.serialise.mock_returns = "mock_page_content"
+        http_server = Mock('OpenIDServer')
+        http_server.PageClass = Mock('PageClass')
+        http_server.PageClass.mock_returns = Page
+        args['server'] = http_server
+        instance = self.make_instance_from_args(args)
+        identity = "fred"
+        expect_stdout = """\
+           Called PageClass(title='...%(identity)s...')
+           ...
+           Called TCPConnection.wfile.write('HTTP/1.0 200 OK\\r\\n')
+           ...
+           Called TCPConnection.wfile.write('\\r\\n')
+           Called TCPConnection.wfile.write('mock_page_content')
+           Called TCPConnection.wfile.close()
+           """ % locals()
+        self.failUnlessOutputCheckerMatch(
+            expect_stdout, self.test_stdout.getvalue()
+        )
 
 
 class Stub_OpenIDRequestHandler(object):
@@ -232,7 +325,6 @@ class Test_OpenIDServer(scaffold.TestCase):
 
     def test_logger_name_as_specified(self):
         """ OpenIDServer should have logger of specified name """
-        import logging
         logger_name_prev = server.logger_name
         logger_name_test = "Foo.Bar"
         server.logger_name = logger_name_test
