da2c26c245c6495834693eb5f09cbf33a85e2075
Ben Finney <ben@benfinney.id.au>
2007-03-15 08:24:47 +1100

    Add forgotten files
    * gracie/test/test_httpserver.py, gracie/httpserver.py:
      - Module for HTTP server behaviour
    
    * gracie/test/__init__.py:
      - Make test directory into a package

---
+++ gracie/httpserver.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+
+# httpserver.py
+# Part of Gracie, an OpenID provider
+#
+# Copyright © 2007 Ben Finney <ben@benfinney.id.au>
+# This is free software; you may copy, modify and/or distribute this work
+# under the terms of the GNU General Public License, version 2 or later.
+# No warranty expressed or implied. See the file LICENSE for details.
+
+""" Behaviour for HTTP server portion of Gracie
+"""
+
+import logging
+from BaseHTTPServer import HTTPServer as BaseHTTPServer
+
+import server
+
+# Name of the Python logging instance to use for this module
+logger_name = "gracie.httpserver"
+
+default_host = "localhost"
+default_port = 8000
+
+
+class BaseHTTPServer(BaseHTTPServer, object):
+    """ Shim to insert base object type into hierarchy """
+
+
+http_port = 80
+
+def net_location(host, port=None):
+    """ Construct a location string from host string and port number """
+    if port is None or port == http_port:
+        location_spec = "%(host)s"
+    else:
+        location_spec = "%(host)s:%(port)s"
+    location = location_spec % locals()
+    return location
+
+
+class HTTPServer(BaseHTTPServer):
+    """ Server for HTTP protocol requests """
+
+    def __init__(self,
+            server_address, RequestHandlerClass, gracie_server
+    ):
+        """ Set up a new instance """
+        self.gracie_server = gracie_server
+        self._setup_version()
+        self._setup_logging()
+        super(HTTPServer, self).__init__(
+            server_address, RequestHandlerClass
+        )
+
+    def _setup_version(self):
+        """ Set up version string for this server """
+        self.version = self.gracie_server.version
+
+    def _setup_logging(self):
+        """ Set up logging for this server """
+        self.logger = logging.getLogger(logger_name)
+        version = self.version
+        self.logger.info(
+            "Starting Gracie HTTP server (version %(version)s)"
+            % locals()
+        )
+
+    def server_bind(self):
+        """ Bind and name the server """
+        super(HTTPServer, self).server_bind()
+        self.server_location = net_location(
+            self.server_name, self.server_port
+        )
+
+    def handle_request(self):
+        """ Handle a single request """
+        try:
+            super(HTTPServer, self).handle_request()
+        except (KeyboardInterrupt, SystemExit), e:
+            exc_name = e.__class__.__name__
+            message = "Received %(exc_name)s" % locals()
+            self.logger.warn(message)
+            raise
+        except Exception, e:
+            message = str(e)
+            self.logger.error(message)
+            raise
---
+++ gracie/test/test_httpserver.py
@@ -0,0 +1,171 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# test_httpserver.py
+# Part of Gracie, an OpenID provider
+#
+# Copyright © 2007 Ben Finney <ben+python@benfinney.id.au>
+# This is free software; you may copy, modify and/or distribute this work
+# under the terms of the GNU General Public License, version 2 or later.
+# No warranty expressed or implied. See the file LICENSE for details.
+
+""" Unit test for httpserver module
+"""
+
+import sys
+import os
+from StringIO import StringIO
+
+import scaffold
+from scaffold import Mock
+from test_httpresponse import Stub_ResponseHeader, Stub_Response
+from test_server import stub_server_bind, make_default_opts
+from test_gracie import Stub_GracieServer
+
+import httpserver
+
+
+class Test_net_location(scaffold.TestCase):
+    """ Test cases for net_location function """
+
+    def test_combines_components_to_location(self):
+        """ net_location() should combine components to net location """
+        locations = {
+            ("foo", None): "foo",
+            ("foo", 80): "foo",
+            ("foo", 81): "foo:81",
+        }
+
+        for (host, port), expect_location in locations.items():
+            location = httpserver.net_location(host, port)
+            self.failUnlessEqual(expect_location, location)
+
+
+class Stub_ConsumerAuthStore(object):
+    """ Stub class for ConsumerAuthStore """
+
+    def __init__(self):
+        self._authorisations = dict()
+
+    def store_authorisation(self, auth_tuple, status):
+        self._authorisations[auth_tuple] = status
+
+    def is_authorised(self, auth_tuple):
+        return self._authorisations.get(auth_tuple, False)
+
+class Stub_ConsumerAuthStore_always_auth(Stub_ConsumerAuthStore):
+    """ ConsumerAuthStore stub that always authorises """
+
+    def is_authorised(self, auth_tuple):
+        return True
+
+class Stub_ConsumerAuthStore_never_auth(Stub_ConsumerAuthStore):
+    """ ConsumerAuthStore stub that never authorises """
+
+    def is_authorised(self, auth_tuple):
+        return False
+
+
+class Stub_HTTPRequestHandler(object):
+    """ Stub class for HTTPRequestHandler """
+
+
+class Test_HTTPServer(scaffold.TestCase):
+    """ Test cases for HTTPServer class """
+
+    def setUp(self):
+        """ Set up test fixtures """
+
+        self.server_class = httpserver.HTTPServer
+
+        self.stub_handler_class = Stub_HTTPRequestHandler
+        self.mock_handler_class = Mock('HTTPRequestHandler')
+
+        self.server_bind_prev = httpserver.BaseHTTPServer.server_bind
+        httpserver.BaseHTTPServer.server_bind = stub_server_bind
+
+        self.stdout_prev = sys.stdout
+        self.stdout_test = StringIO()
+        sys.stdout = self.stdout_test
+
+        self.valid_servers = {
+            'simple': dict(
+                address = ('', 80),
+            ),
+        }
+
+        for key, params in self.valid_servers.items():
+            args = params.get('args')
+            address = params.get('address')
+            handler_class = params.setdefault(
+                'handler_class', self.stub_handler_class)
+            opts = make_default_opts()
+            gracie_server = params.setdefault(
+                'gracie_server', Stub_GracieServer(address, opts)
+            )
+            if not args:
+                args = dict(
+                    server_address = params['address'],
+                    RequestHandlerClass = params['handler_class'],
+                    gracie_server = params['gracie_server']
+                )
+            instance = self.server_class(**args)
+            params['args'] = args
+            params['instance'] = instance
+
+        self.iterate_params = scaffold.make_params_iterator(
+            default_params_dict = self.valid_servers
+        )
+
+    def tearDown(self):
+        """ Tear down test fixtures """
+        sys.stdout = self.stdout_prev
+        httpserver.BaseHTTPServer.server_bind = self.server_bind_prev
+
+    def test_instantiate(self):
+        """ New HTTPServer instance should be created """
+        for key, params in self.iterate_params():
+            instance = params['instance']
+            self.failUnless(instance is not None)
+
+    def test_version_as_specified(self):
+        """ HTTPServer should have specified version string """
+        params = self.valid_servers['simple']
+        version_prev = httpserver.server.__version__
+        version_test = Stub_GracieServer.version
+        httpserver.server.__version__ = version_test
+        instance = self.server_class(**params['args'])
+        self.failUnlessEqual(version_test, instance.version)
+        httpserver.server.__version__ = version_prev
+
+    def test_request_handler_class_as_specified(self):
+        """ HTTPServer should have specified RequestHandlerClass """
+        for key, params in self.iterate_params():
+            instance = params['instance']
+            handler_class = params['handler_class']
+            self.failUnlessEqual(
+                handler_class, instance.RequestHandlerClass
+            )
+
+    def test_gracie_server_as_specified(self):
+        """ HTTPServer should have specified Gracie server instance """
+        for key, params in self.iterate_params():
+            instance = params['instance']
+            gracie_server = params['gracie_server']
+            self.failUnlessEqual(
+                gracie_server, instance.gracie_server
+            )
+
+    def test_serve_forever_is_callable(self):
+        """ HTTPServer.serve_forever should be callable """
+        self.failUnless(callable(self.server_class.serve_forever))
+
+
+suite = scaffold.suite(__name__)
+
+__main__ = scaffold.unittest_main
+
+if __name__ == '__main__':
+    import sys
+    exitcode = __main__(sys.argv)
+    sys.exit(exitcode)
