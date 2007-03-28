3ba643f797ab92c81878f4078a6b0584632c78fc
Ben Finney <ben@benfinney.id.au>
2007-03-28 23:04:36 +1000

    More Python setuptools packaging
    * src/Makefile:
      - Add targets and actions for:
        register, sdist, bdist, upload, dist
    
    * src/setup.py:
      - Install our programs via install_scripts
    
    * .bzrignore:
      - Ignore autogenerated MANIFEST{,.in}

--- src/setup.py
+++ src/setup.py
@@ -15,19 +15,54 @@
 import ez_setup
 ez_setup.use_setuptools()
 
+import sys
+import os
+import stat
+
 from setuptools import setup, find_packages
+from setuptools import command as st_command
+from setuptools.command.install_scripts import install_scripts
 
 main_module_name = 'gracie'
 main_module = __import__(main_module_name)
 
 description = main_module.__doc__.split('\n\n', 1)
 
+
+class gracie_install_scripts(install_scripts):
+    """ Custom install_scripts command for Gracie """
+
+    def run(self):
+        """ Run the command """
+        self.mkpath(os.path.normpath(self.install_dir))
+
+        self.outfiles = []
+        _p = os.path.join
+        scripts = {
+            _p(self.build_dir, "gracied"): self.install_dir,
+        }
+        for src_file, dest_dir in scripts.items():
+            ofile, copied = self.copy_file(src_file, dest_dir)
+            if copied:
+                self.outfiles.append(ofile)
+
+        if os.name == "posix":
+            for ofile in self.get_outputs():
+                if not self.dry_run:
+                    mode = os.stat(ofile)[stat.ST_MODE]
+                    new_mode = (mode | 0555) & 07777
+                    os.chmod(ofile, new_mode)
+
+
 setup(
     name = main_module_name,
     version = main_module.__version__,
     packages = find_packages(
         exclude = ['test'],
     ),
+    cmdclass = {
+        'install_scripts': gracie_install_scripts,
+    },
 
     # setuptools metadata
     zip_safe = True,
