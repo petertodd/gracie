1350f4a78eed04ca84c2ecb465b14241187ecdc3
Ben Finney <ben@benfinney.id.au>
2008-06-30 17:33:37 +1000

    Implement continuous unit test loop.

--- test/module.mk
+++ test/module.mk
@@ -19,11 +19,44 @@ GENERATED_FILES += .coverage
 PYFLAKES = pyflakes
 PYLINT = pylint
 COVERAGE = python-coverage
+DATE = date
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+
+INWAIT = inotifywait
+inwait_events_opts =
+inwait_events_opts += -e create
+inwait_events_opts += -e modify
+inwait_events_opts += -e delete
+inwait_exclude_regex = '(\#.+\#|\..+\.sw[[:alpha:]])$$'
+INWAIT_OPTS = -r -t 0 ${inwait_events_opts} --exclude ${inwait_exclude_regex}
+TEST_INWAIT_FILES = ${CODE_PACKAGE_DIRS} ${CODE_PROGRAMS} ${TEST_DIR}
+
+
+# usage: $(call test-output-banner,message)
+define test-output-banner
+	@ echo -n ${1} ; \
+	$(DATE) +${DATE_FORMAT}
+endef
 
 
 test:
+	$(call test-output-banner, "Test run: " )
 	$(PYTHON) ${TEST_SUITE}
 
+# usage: $(call test-wait)
+define test-wait
+	$(INWAIT) ${INWAIT_OPTS} ${TEST_INWAIT_FILES}
+endef
+
+.PHONY: test-continuous
+test-continuous:
+	while true ; do \
+		clear ; \
+		$(MAKE) test ; \
+		$(call test-wait) ; \
+	done
+
+
 .PHONY: pyflakes
 pyflakes:
 	$(PYFLAKES) .
